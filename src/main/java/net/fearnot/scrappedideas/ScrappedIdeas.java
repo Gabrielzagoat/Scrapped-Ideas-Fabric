ab53613045ca910f5c4b6240a83270c90662a5d7
Fearnot <gabitmg11@gmail.com>
2024-12-15 14:34:37 -0300

    There is nothing in the Mod yet. This is just the basi setup for it.

---
+++ src/main/java/net/fearnot/scrappedideas/ScrappedIdeas.java
@@ -0,0 +1,16 @@
+package net.fearnot.scrappedideas;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScrappedIdeas implements ModInitializer {
+	public static final String MOD_ID = "scrappedideas";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fearnot/scrappedideas/ScrappedIdeasClient.java
@@ -0,0 +1,10 @@
+package net.fearnot.scrappedideas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScrappedIdeasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
